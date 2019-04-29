e6cb180f95033f6c2d9a31872edf2ec44d5acdb4
Michel <Michel@MICHEL.lan>
2019-04-29 18:19:49 +0200

    3

---
+++ controller/AdminController.php
@@ -0,0 +1,100 @@
+<?php
+
+// CLASSES CHARGEES PAR AUTOLOAD.
+
+namespace controller;
+
+class AdminController extends Controller {
+
+    private static $_instance = null;
+
+    private function __construct() {
+        
+    }
+
+    public static function getInstance() {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new AdminController();
+        }
+        return self::$_instance;
+    }
+
+    public function mainMenu() {
+
+        $this->getViewContent('mainMenu', array(), 'template');
+    }
+
+    public function controlSession() {
+        if (filter_has_var(INPUT_POST, 'username') && filter_has_var(INPUT_POST, 'password')) {
+            $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+            $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+            try {
+                $adminDAO = new \model\AdminDAO();
+
+                $user = $adminDAO->selectUser($username, $password, 'yes');
+                if ($user != null) {
+                    $_SESSION["user"] = serialize($user);
+                    $bu = $user->getUser_default_bu();
+
+                    if ($bu != 0)
+                        $_SESSION["bu"] = $bu;
+                    if (filter_has_var(INPUT_POST, 'rememberme')) {
+                        setcookie('CAL1', $user->getUser_pseudo(), time() + 365 * 24 * 3600, null, null, false, true);
+                        setcookie('CAL2', $user->getUser_password(), time() + 365 * 24 * 3600, null, null, false, true);
+                    }
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+        if (!isset($_SESSION['user'])) {
+            if (isset($_COOKIE['CAL1']) && isset($_COOKIE['CAL2'])) {
+                try {
+                    $adminDAO = new \model\AdminDAO();
+                    $user = $adminDAO->selectUser($_COOKIE['CAL1'], $_COOKIE['CAL2'], '');
+                    if ($user != null) {
+                        $_SESSION["user"] = serialize($user);
+                        $bu = $user->getUser_default_bu();
+                        if ($bu != 0)
+                            $_SESSION["bu"] = $bu;
+                        return true;
+                        header("location:/routes.php");
+                    }else {
+                        // $messageErreur='Identifiant ou mot de passe erroné';
+                       // require('view/frontend/login.php');
+                         $this->getViewContent('login', array(), 'template');
+                        return false;
+                    }
+                } catch (Exception $e) {
+                    echo '<h1>Erreur : ' . $e->getMessage() . '</h1>';
+                }
+            } else {
+                $messageErreur='';
+                if (filter_has_var(INPUT_POST, 'username') && filter_has_var(INPUT_POST, 'password')){
+                    $messageErreur = 'Identifiant ou mot de passe erroné';}
+                  $this->getViewContent('login', array('messageErreur'=>$messageErreur), 'template');
+               // require('view/frontend/login.php');
+                return false;
+            }
+        } else {
+            return true;
+            header("location:/routes.php");
+        }
+    }
+
+    public function disconnectUser() {
+        session_destroy();
+        setcookie("CAL1", "", time() - 3600);
+        setcookie("CAL2", "", time() - 3600);
+        header("location:/routes.php");
+    }
+
+    function manageUserSession() {
+        $this->getViewContent('login', array(), 'template');
+    }
+
+    public function changeBU($bu) {
+        $_SESSION['bu'] = $bu;
+    }
+
+}
---
+++ model/DBAccess.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace model;
+
+// CLASSE SINGLETON
+
+class DBAccess {
+
+    private $PDOInstance = null;
+    private static $instance = null;
+
+    public function __construct() {
+        $properties = parse_ini_file("controller/connect.properties");
+        $protocole = $properties["protocole"];
+        $serveur = $properties["serveur"];
+        $port = $properties["port"];
+        $user = $properties["user"];
+        $mdp = $properties["mdp"];
+        $db = $properties["bd"];
+        try {
+            $this->PDOInstance = new \PDO("$protocole:host=$serveur;"
+                    . "dbname=$db;charset=utf8", $user, $mdp);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public static function getDBInstance() {
+        if (is_null(self::$instance)) {
+            self::$instance = new DBAccess();
+        }
+        return self::$instance;
+    }
+
+    protected function dbClose(\PDO $db) {
+        try {
+            $db = null;
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+
+    public function prepare($sql) {
+        return $this->PDOInstance->prepare($sql);
+    }
+    
+    public function query($sql) {
+        return $this->PDOInstance->query($sql);
+    }
+
+    public function inTransaction() {
+        return $this->PDOInstance->inTransaction();
+    }
+
+    public function beginTransaction() {
+        return $this->PDOInstance->beginTransaction();
+    }
+
+    public function commit() {
+        return $this->PDOInstance->commit();
+    }
+
+    public function lastInsertId() {
+        return $this->PDOInstance->lastInsertId();
+    }
+    
+}
