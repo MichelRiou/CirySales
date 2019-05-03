f5cb69e9c1bb569288dadf2aae074a57fc44de16
Michel <Michel@192.168.0.10>
2019-05-03 09:46:46 +0200

    3 commit Home

--- view/webcustomer2.php
+++ view/webcustomer2.php
@@ -1,26 +1,26 @@
-<div class="container">
+<div class="container py-5 extra">
   <h2>Horizontal form</h2>
-  <form class="form-horizontal">
-    <div class="form-group">
+  <form>
+    <div class="form-group row">
       <label class="control-label col-sm-2" for="email">Email:</label>
-      <div class="col-sm-10">
+      <div class="col-sm-8">
         <input type="email" class="form-control" id="email" placeholder="Enter email">
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group row">
       <label class="control-label col-sm-2" for="pwd">Password:</label>
-      <div class="col-sm-10">          
+      <div class="col-sm-8">          
         <input type="password" class="form-control" id="pwd" placeholder="Enter password">
       </div>
     </div>
-    <div class="form-group">        
+    <div class="form-group row">        
       <div class="col-sm-offset-2 col-sm-10">
         <div class="checkbox">
           <label><input type="checkbox"> Remember me</label>
         </div>
       </div>
     </div>
-    <div class="form-group">        
+    <div class="form-group row">        
       <div class="col-sm-offset-2 col-sm-10">
         <button type="submit" class="btn btn-default">Submit</button>
       </div>
