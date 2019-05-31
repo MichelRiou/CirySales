145ff6c2a3b67daf71818c8c240312fb011c8b31
Michel <Michel@MICHEL>
2019-05-31 17:40:53 +0200

    1er commit après méchant crash...:(

--- view/mainMenu.php
+++ view/mainMenu.php
@@ -19,10 +19,11 @@
 
                                 <div class="form-group encart mx-auto ">
                                     <div>
-                                        <h2><p>VENTE DE PRET A PORTER</p></h2>
+                                        <h2><p>VENTE DE PRET A PORTER<br>
+                                        FEMININ ET MASCULIN</p></h2>
                                     </div>
                                     <div>
-                                        <h3> <p>LES 16 ET 17 MAI 2019</p>
+                                        <h3> <p>Vendredi 15 et samedi 16 novembre 2019</p>
                                         <p>SALLE DES FETES</p>
                                         <p>CIRY LE NOBLE</p></h3>
                                     </div>
